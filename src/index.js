21a81b4667953fc91a050bceec228a0e2a3afa0e
jiaoyuan <18222595056@163.com>
2019-10-11 20:51:47 +0800

    1

---
+++ src/index.js
@@ -0,0 +1,14 @@
+/*
+ * @Description: In User Settings Edit
+ * @Author: your name
+ * @Date: 2019-10-11 15:49:41
+ * @LastEditTime: 2019-10-11 15:49:41
+ * @LastEditors: your name
+ */
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './TodoList';
+
+ReactDOM.render(<App />, document.getElementById('root'));
+
+
